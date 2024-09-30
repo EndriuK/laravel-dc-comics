0c6ba77bb0d29d67c317053912837bf38b3422a3
EndriuK <157508097+EndriuK@users.noreply.github.com>
2024-09-30 18:22:12 +0200

    [UPD] completata milestone 1

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,3 +1,5 @@
+
+
 @extends('layouts.app')
 
 @section('content')
@@ -5,12 +7,23 @@
     <h1 class="mb-4">Comics List</h1>
     <ul class="list-group">
         @foreach ($comics as $comic)
-            <li class="list-group-item">
+            <li class="list-group-item d-flex justify-content-between align-items-center">
                 <a href="{{ route('comics.show', $comic->id) }}" class="text-decoration-none">
                     {{ $comic->title }} - {{ $comic->price }} €
                 </a>
+                <form action="{{ route('comics.destroy', $comic->id) }}" method="POST" onsubmit="return confirmDelete()">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                </form>
             </li>
         @endforeach
     </ul>
 </div>
+
+<script>
+    function confirmDelete() {
+        return confirm('Are you sure you want to delete this comic?');
+    }
+</script>
 @endsection
